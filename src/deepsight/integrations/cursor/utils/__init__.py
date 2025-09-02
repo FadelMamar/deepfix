352d12bfe77b5dcdb42fbcfb6e9c447e02a904c8
fadel <fadel>
2025-09-02 18:42:37 +0200

    delete

--- src/deepsight/integrations/cursor/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Utility modules for Cursor CLI wrapper."""
-
-from .process import ProcessManager
-from .errors import CursorError, CursorTimeoutError, CursorNotFoundError
-
-__all__ = ["ProcessManager", "CursorError", "CursorTimeoutError", "CursorNotFoundError"]
